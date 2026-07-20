e5705bb9133b4980af12972f8134b28b562e17bd
alefe-redes-computadores <202509217108@alunos.estacio.br>
2026-07-20 14:48:13 -0300

    Atualizar o firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,7 +2,6 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
-
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
